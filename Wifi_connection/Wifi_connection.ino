b066d4cf3baca196685b3ebf526c56dc72be1113
Tim_dell <azertyuioiuytrezazertyuio@gmail.com>
2026-01-01 15:40:47 +0100

    led test

--- Wifi_connection/Wifi_connection.ino
+++ Wifi_connection/Wifi_connection.ino
@@ -1,9 +1,16 @@
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
 
 const char* ssid = "Arduino_AP";
 const char* password = "12345678";
 
-void setup() {
+WiFiUDP udp;
+
+char packet[255];
+
+void setup(){
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("Hello");
   WiFi.begin(ssid, password);
@@ -16,9 +23,18 @@ void setup() {
   Serial.print("IP : ");
   Serial.println(WiFi.localIP());
 
-
+  udp.begin(4210);
+  delay(500);
 }
 
 void loop() {
-
+  int len = udp.parsePacket();
+  if (len) {
+    udp.read(packet, 255);
+    packet[len] = 0;
+    Serial.println(packet);
+    if (strcmp(packet, "led1 on")==0){digitalWrite(LED_BUILTIN, LOW);}
+    if (strcmp(packet, "led1 off")==0){digitalWrite(LED_BUILTIN, HIGH);}
+  }
+  delay(5);time(nullptr);
 }
--- Wifi_creation/Wifi_creation.ino
+++ Wifi_creation/Wifi_creation.ino
@@ -1,16 +1,56 @@
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <user_interface.h>
+
+
+WiFiUDP udp;
+
 
 const char* ssid = "Arduino_AP";
 const char* password = "12345678";
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
   Serial.begin(115200);
   WiFi.softAP(ssid, password);
 
   Serial.print("IP du point d'accès : ");
   Serial.println(WiFi.softAPIP());
+
+  udp.begin(4210);
 }
 
 void loop() {
-
+  envoi("led1 on");
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  envoi("led1 off");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
 }
+
+void envoi(const char* message){
+  udp.beginPacket("192.168.4.255", 4210); //IP Broadcast adress - to send packet to everyone
+  udp.print(message);
+  udp.endPacket();
+}
+
+/*
+IPAddress afficherClients[10]() {
+  struct station_info *stat_info = wifi_softap_get_station_info();
+  struct station_info *it = stat_info;
+  IPAddress ips[15];
+  i=0;
+  while (it != NULL) {
+    IPAddress ip = IPAddress(it->ip.addr);
+    Serial.print("Client connecté : ");
+    Serial.println(ip);
+    ips[i] = ip;
+    it = STAILQ_NEXT(it, next);
+    i++;
+  }
+  wifi_softap_free_station_info();
+  return ips;
+}/**/
